aaa703f98c6948ef987440e1c6b21df6bba0e1be
fernando villamor jr <fervillz@gmail.com>
2016-03-12 11:28:34 -0800

    first files

---
+++ widget_format.php
@@ -0,0 +1,18 @@
+<?php
+class Tutsplus_List_Pages_Widget extends WP_Widget {
+     
+    function __construct() {
+    }
+     
+    function form( $instance ) {
+    }
+     
+    function update( $new_instance, $old_instance ) {       
+    }
+     
+    function widget( $args, $instance ) {
+         
+    }
+     
+}
+?>
